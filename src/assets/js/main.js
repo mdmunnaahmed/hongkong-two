f4df9912f47a1278e08c13661e26927d050ab512
mu2na <msdmunna77@gmail.com>
2022-08-31 08:05:52 +0600

    gallery done profile running -munna

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -102,61 +102,6 @@ $(".scrollToTop").on("click", function () {
 	return false;
 });
 
-$(".brand-slider").slick({
-	fade: false,
-	slidesToShow: 6,
-	slidesToScroll: 1,
-	infinite: true,
-	autoplay: true,
-	pauseOnHover: true,
-	centerMode: false,
-	dots: false,
-	arrows: false,
-	nextArrow: '<i class="las la-arrow-right arrow-right"></i>',
-	prevArrow: '<i class="las la-arrow-left arrow-left"></i> ',
-	responsive: [
-		{
-			breakpoint: 1199,
-			settings: {
-				slidesToShow: 5,
-			},
-		},
-		{
-			breakpoint: 992,
-			settings: {
-				slidesToShow: 5,
-			},
-		},
-		{
-			breakpoint: 767,
-			settings: {
-				slidesToShow: 4,
-			},
-		},
-		{
-			breakpoint: 575,
-			settings: {
-				slidesToShow: 3,
-			},
-		},
-	],
-});
-
-// Odometer Counter
-let counter = $(".counter-item");
-if (counter) {
-	counter.each(function () {
-		$(this).isInViewport(function (status) {
-			if (status === "entered") {
-				for (var i = 0; i < document.querySelectorAll(".odometer").length; i++) {
-					var el = document.querySelectorAll(".odometer")[i];
-					el.innerHTML = el.getAttribute("data-odometer-final");
-				}
-			}
-		});
-	});
-}
-
 // Shop Cart
 $(".minus-plus").on("click", function () {
 	var oldValue = $(this).siblings("input").val();
@@ -197,129 +142,6 @@ $(".menu li a").each(function () {
 	}
 });
 
-// Gallery Modal
-$(".overlay, .modal-close, .modal-cancel").on("click", function (e) {
-	$(".overlay").removeClass("active-color");
-	$(".gallery-modal").removeClass("active");
-	$(".share-modal").removeClass("active");
-	$(".success-modal").removeClass("active");
-	$(".logout-modal").removeClass("active");
-});
-// Gallery Modal
-
-// Gallery Modal Click Event
-let trigger3 = $(".gallery-item");
-let dropdown3 = $(".gallery-modal");
-if (trigger3 || dropdown3) {
-	trigger3.each(function () {
-		$(this).on("click", function (e) {
-			e.stopPropagation();
-			dropdown3.toggleClass("active");
-			$(".overlay").toggleClass("active-color");
-		});
-	});
-	dropdown3.each(function () {
-		$(this).on("click", function (e) {
-			e.stopPropagation();
-		});
-	});
-	$(document).on("click", function () {
-		dropdown3.removeClass("active");
-		$(".overlay").addClass("active-color");
-	});
-}
-
-// Share Modal Click Event
-let trigger4 = $(".share-btn");
-let dropdown4 = $(".share-modal");
-if (trigger4 || dropdown4) {
-	trigger4.each(function () {
-		$(this).on("click", function (e) {
-			e.stopPropagation();
-			dropdown4.toggleClass("active");
-			dropdown3.removeClass("active");
-			$(".overlay").addClass("active-color");
-		});
-	});
-	dropdown4.each(function () {
-		$(this).on("click", function (e) {
-			e.stopPropagation();
-		});
-	});
-	$(document).on("click", function () {
-		dropdown4.removeClass("active");
-		$(".overlay").removeClass("active-color");
-	});
-}
-
-// Downaload Modal Click Event
-let trigger5 = $(".download-btn");
-let dropdown5 = $(".success-modal");
-if (trigger5 || dropdown5) {
-	trigger5.each(function () {
-		$(this).on("click", function (e) {
-			e.stopPropagation();
-			dropdown3.removeClass("active");
-			dropdown5.toggleClass("active");
-			$(".overlay").addClass("active-color");
-		});
-	});
-	dropdown5.each(function () {
-		$(this).on("click", function (e) {
-			e.stopPropagation();
-		});
-	});
-	$(document).on("click", function () {
-		dropdown5.removeClass("active");
-		$(".overlay").removeClass("active-color");
-	});
-}
-
-// Logout Modal Click Event
-let trigger6 = $(".logout-btn");
-let dropdown6 = $(".logout-modal");
-if (trigger6 || dropdown6) {
-	trigger6.each(function () {
-		$(this).on("click", function (e) {
-			e.stopPropagation();
-			dropdown3.removeClass("active");
-			dropdown6.toggleClass("active");
-			$(".overlay").addClass("active-color");
-		});
-	});
-	dropdown6.each(function () {
-		$(this).on("click", function (e) {
-			e.stopPropagation();
-		});
-	});
-	$(document).on("click", function () {
-		dropdown6.removeClass("active");
-		$(".overlay").removeClass("active-color");
-	});
-}
-
-// Logout Modal Click Event
-let trigger7 = $(".buy-package");
-let dropdown7 = $(".package-modal");
-if (trigger7 || dropdown7) {
-	trigger7.each(function () {
-		$(this).on("click", function (e) {
-			e.stopPropagation();
-			dropdown3.removeClass("active");
-			dropdown7.toggleClass("active");
-			$(".overlay").addClass("active-color");
-		});
-	});
-	dropdown7.each(function () {
-		$(this).on("click", function (e) {
-			e.stopPropagation();
-		});
-	});
-	$(document).on("click", function () {
-		dropdown7.removeClass("active");
-		$(".overlay").removeClass("active-color");
-	});
-}
 
 $(".logo").on("click", function (e) {
 	$(".menu-sidebar").toggleClass("menu-icon-show");
@@ -340,3 +162,20 @@ window.addEventListener("resize", function (e) {
 
 var menuBottomWidth = $(".responsive-bottom-menu").height();
 $(".body-content").css("margin-bottom", menuBottomWidth + 40 + "px");
+
+
+$("#photoVideo").on("change", function () {
+	if ($("#photos").hasClass("active")) {
+		$("#photos").toggleClass("active show");
+		$("#videos").toggleClass("active show");
+		console.log(1234);
+	}
+});
+
+$("#photoVideo").on("change", function () {
+	if ($("#videos").hasClass("active")) {
+		$("#videos").toggleClass("active show");
+		$("#photos").toggleClass("active show");
+		console.log(000);
+	}
+});
